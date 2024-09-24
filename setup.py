41105bba786b353a156c6ba13de498158ecfb72c
Alex Alderman Webb <s2186366@saruman.inf.ed.ac.uk>
2024-09-24 11:16:27 +0100

    build: change build system to colcon and update package structure

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'pf_localisation'
 
-d = generate_distutils_setup(
-   packages=['pf_localisation'],
-   package_dir={'': 'src'}
+setup(
+    name=package_name,
+    version='3.0.0',
+    packages=['pf_localisation'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Alex Alderman Webb',
+    maintainer_email='s2186366@ed.ac.uk',
+    description='Particle filter assignment for localisation in a map.',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'pf_node = pf_localisation.node:main',
+        ]
+    }
 )
-
-setup(**d)
