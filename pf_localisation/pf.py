5c842728157230faca77f74a2b2d3876c47655ec
s2316829 <148646735+s2316829@users.noreply.github.com>
2024-10-18 15:43:00 +0100

    Update pf.py

--- pf_localisation/pf.py
+++ pf_localisation/pf.py
@@ -1,7 +1,10 @@
 from geometry_msgs.msg import Pose, PoseArray, Quaternion, Point
 from . pf_base import PFLocaliserBase
-
 from . util import rotateQuaternion, getHeading
+import random
+import numpy as np
+from .sensor_model import SensorModel
+import rospy
 
 
 class PFLocaliser(PFLocaliserBase):
@@ -11,54 +14,103 @@ class PFLocaliser(PFLocaliserBase):
         super(PFLocaliser, self).__init__()
         
         # ----- Set motion model parameters
- 
+        self.ODOM_ROTATION_NOISE = 0.05
+        self.ODOM_TRANSLATION_NOISE = 0.02
+        self.ODOM_DRIFT_NOISE = 0.01
         # ----- Sensor model parameters
         self.NUMBER_PREDICTED_READINGS = 20     # Number of readings to predict
         
-       
+
+    
     def initialise_particle_cloud(self, initialpose):
         """
-        Set particle cloud to initialpose plus noise
-
-        Called whenever an initialpose message is received (to change the
-        starting location of the robot), or a new occupancy_map is received.
-        self.particlecloud can be initialised here. Initial pose of the robot
-        is also set here.
-        
-        :Args:
-            | initialpose: the initial pose estimate
-        :Return:
-            | (geometry_msgs.msg.PoseArray) poses of the particles
+        Called when setting the initial position of the particle cloud.
         """
-        pass
+        particle_cloud = PoseArray()
+        particle_cloud.header = initialpose.header
 
- 
-    
+        for _ in range(self.num_particles):
+            # Create each particle with some noise around the initial pose
+            particle = Pose()
+
+            # Random Gaussian noise around the initial pose
+            particle.position.x = initialpose.pose.pose.position.x + random.gauss(0, 0.5)
+            particle.position.y = initialpose.pose.pose.position.y + random.gauss(0, 0.5)
+            particle.position.z = 0.0  # assuming the robot is on a flat surface
+
+            # Random noise for orientation
+            yaw_noise = random.gauss(0, 0.1)
+            particle.orientation = rotateQuaternion(initialpose.pose.pose.orientation, yaw_noise)
+
+            particle_cloud.poses.append(particle)
+
+        self.particlecloud = particle_cloud
+        return particle_cloud
+
+
     def update_particle_cloud(self, scan):
         """
-        This should use the supplied laser scan to update the current
-        particle cloud. i.e. self.particlecloud should be updated.
-        
-        :Args:
-            | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
+        Called whenever a new LaserScan message is received.
+        """
+        weights = []
 
-         """
-        pass
+        # Calculate weight for each particle based on the laser scan data
+        for particle in self.particlecloud.poses:
+            weight = self.sensor_model.get_weight(scan, particle)
+            weights.append(weight)
+
+        # Normalize weights to create a probability distribution
+        total_weight = sum(weights)
+        if total_weight == 0:
+            rospy.logwarn("All particles have zero weight, reinitializing.")
+            weights = [1.0 / self.num_particles] * self.num_particles
+        else:
+            weights = [weight / total_weight for weight in weights]
+
+        # Resample the particles based on their weights
+        new_particle_cloud = PoseArray()
+        new_particle_cloud.header = self.particlecloud.header
+
+        indices = np.random.choice(range(self.num_particles), size=self.num_particles, p=weights)
+        for index in indices:
+            selected_particle = self.particlecloud.poses[index]
+
+            # Add some noise to keep particle diversity
+            new_particle = Pose()
+            new_particle.position.x = selected_particle.position.x + random.gauss(0, 0.1)
+            new_particle.position.y = selected_particle.position.y + random.gauss(0, 0.1)
+            new_particle.orientation = rotateQuaternion(selected_particle.orientation, random.gauss(0, 0.05))
+
+            new_particle_cloud.poses.append(new_particle)
+
+        self.particlecloud = new_particle_cloud
+
 
     def estimate_pose(self):
         """
-        This should calculate and return an updated robot pose estimate based
-        on the particle cloud (self.particlecloud).
-        
-        Create new estimated pose, given particle cloud
-        E.g. just average the location and orientation values of each of
-        the particles and return this.
-        
-        Better approximations could be made by doing some simple clustering,
-        e.g. taking the average location of half the particles after 
-        throwing away any which are outliers
+        Estimate the robot's pose from the particle cloud.
+        """
+        # Calculate the weighted average of all particles to determine the robot's pose
+        average_pose = Pose()
+        x_sum = 0.0
+        y_sum = 0.0
+        sin_sum = 0.0
+        cos_sum = 0.0
 
-        :Return:
-            | (geometry_msgs.msg.Pose) robot's estimated pose.
-         """
-        pass
+        for particle in self.particlecloud.poses:
+            x_sum += particle.position.x
+            y_sum += particle.position.y
+
+            # Convert quaternion to yaw to accumulate
+            yaw = getHeading(particle.orientation)
+            sin_sum += np.sin(yaw)
+            cos_sum += np.cos(yaw)
+
+        average_pose.position.x = x_sum / self.num_particles
+        average_pose.position.y = y_sum / self.num_particles
+
+        # Convert back to quaternion
+        average_yaw = np.arctan2(sin_sum, cos_sum)
+        average_pose.orientation = rotateQuaternion(self.particlecloud.poses[0].orientation, average_yaw)
+
+        return average_pose
